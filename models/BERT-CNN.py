015f294ca2bc29ecf58c08d3ab0f2c7612c631be
simpleax <zhangqiuxia268@163.com>
2024-09-10 19:39:55 +0800

    Update BERT-CNN.py

--- models/BERT-CNN.py
+++ models/BERT-CNN.py
@@ -13,37 +13,6 @@ from tqdm import tqdm
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
-def show_confusion_matrix(confusion, classes=["0", "1"], x_rot=-60, figsize=None, save=True):
-    """
-    绘制混淆矩阵
-    :param confusion:
-    :param classes:
-    :param x_rot:
-    :param figsize:
-    :param save:
-    :return:
-    """
-    if figsize is not None:
-        plt.rcParams['figure.figsize'] = figsize
-
-    plt.imshow(confusion, cmap=plt.cm.YlOrRd)
-    indices = range(len(confusion))
-    plt.xticks(indices, classes, rotation=x_rot, fontsize=12)
-    plt.yticks(indices, classes, fontsize=12)
-    plt.colorbar()
-    plt.xlabel('y_pred')
-    plt.ylabel('y_true')
-
-    # 显示数据
-    for first_index in range(len(confusion)):
-        for second_index in range(len(confusion[first_index])):
-            plt.text(first_index, second_index, confusion[first_index][second_index])
-
-    if save:
-        plt.savefig("./bert_cnn_confusion_matrix.png")
-    plt.show()
-
-
 maxlen = 256
 batch_size = 16
 config_path = './model/config.json'
@@ -122,73 +91,4 @@ class Model(BaseModel):
         output = self.dropout(feature_output)
         output = self.dense(output)
         return output
-
-
-model = Model().to(device)
-
-# 定义使用的loss(Cross-Entropy loss)和optimizer
-model.compile(
-    loss=nn.CrossEntropyLoss(),
-    optimizer=optim.AdamW(model.parameters(), lr=2e-5),
-    metrics=['acc'],  # 准确率accuracy
-    metrics2=['pre'],  # 精确率precision
-    metrics3=['rec'],  # 召回率rcall
-    metrics4=['f1']  # f1分数
-)
-
-
-# 定义评价函数
-def evaluate(data, mark):
-    model.eval()
-    total, right = 0., 0.
-    pre = []
-    gro = []
-    for x_true, y_true in tqdm(data):
-        y_pred = model.predict(x_true).argmax(axis=1)
-        pre += y_pred.tolist()
-        gro += y_true.tolist()
-        total += len(y_true)
-        right += (y_true == y_pred).sum().item()
-    precise = metrics.precision_score(gro, pre)
-    rec = metrics.recall_score(gro, pre)
-    f1 = metrics.f1_score(gro, pre)
-    print(f'{mark} precision: {precise:.5f}, recall: {rec:.5f}, f1: {f1:.5f}, acc: {right / total:.5f}')
-    model.train()
-    return right / total
-
-
-class Evaluator(Callback):
-    """评估与保存
-    """
-    def __init__(self):
-
-        self.best_val_acc = 0.
-
-    def on_epoch_end(self, global_step, epoch, logs=None):
-        train_acc = evaluate(train_dataloader, "train")
-
-        test_acc = evaluate(test_dataloader, "test")
-        if test_acc > self.best_val_acc:
-            self.best_val_acc = test_acc
-            model.save_weights('./BERT_CNN/best_model.pt')
-        print(f'val_acc: {test_acc:.5f}, best_val_acc: {self.best_val_acc:.5f}\n')
-
-
-if True:
-    evaluator = Evaluator()
-    model.fit(train_dataloader, epochs=20, callbacks=[evaluator])
-
-model.load_weights('./BERT_CNN/best_model.pt')
-evaluate(test_dataloader, "test")
-
-pre = []
-gro = []
-for x_true, y_true in tqdm(test_dataloader):
-    y_pred = model.predict(x_true).argmax(axis=1)
-    pre += y_pred.tolist()
-    gro += y_true.tolist()
-
-confusion = metrics.confusion_matrix(gro, pre, labels=None)
-print(confusion)
-show_confusion_matrix(confusion)
 
